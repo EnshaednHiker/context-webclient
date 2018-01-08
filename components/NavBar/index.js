1b028eaed2fd77bb06e6c62ed801c89ab3b6ca1b
EnshaednHiker <nbingham09@gmail.com>
2018-01-07 18:18:00 -0600

    combined dash component functionality in navbar component, navbar works across views

--- components/NavBar/index.js
+++ components/NavBar/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import Dom from 'react-dom';
-
 import { connect } from 'react-redux';
-const ClickOutHandler = require('react-onclickout');
-import {scrollAtTop, hamburgerClick, collapseMenu, logout } from '~/actions'
+import {scrollAtTop, hamburgerClick, logout, setAnnotationString, annotate, clearAnnotation} from '~/actions'
 
 import '~/assets/styles/main.css'
 
 import Scroll from 'react-scroll';
-import MediaQuery from 'react-responsive';
-
 
 const Element = Scroll.Element;
 const Link = Scroll.Link;
@@ -20,12 +16,11 @@ export class NavBar extends React.Component {
         super(props)
         
         this.scrollToTop = this.scrollToTop.bind(this);
-        this.handleHamburgerClick = this.handleHamburgerClick.bind(this);
         this.handleLogoutClick = this.handleLogoutClick.bind(this);
         this.handleGoToOtherPage = this.handleGoToOtherPage.bind(this);
-        //this.handleScroll = this.handleScroll.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        
     }
-
 
     componentDidMount() {
         document.addEventListener('scroll', () => {
@@ -33,21 +28,18 @@ export class NavBar extends React.Component {
             this.props.dispatch(scrollAtTop(boolean));
         });
     }
-
-    onClickOut() {
-
-       // console.log("this.props._owner._instance: ",this.props.children._owner._instance.props);
-       // this.props.children._owner._instance.props.dispatch(collapseMenu('click'));
-    }
-    // handleScroll () {
-    //     //onScroll={this.handleScroll}
-    //     let boolean = window.scrollY < 1;
-    //     this.props.dispatch(scrollAtTop(boolean));       
-    // }
-
-    handleHamburgerClick () {
-        let boolean = this.props.isCollapsed;
-        this.props.dispatch(hamburgerClick(!boolean));
+    handleSubmit(e, annotation){
+        if(annotation === null){
+            e.preventDefault();
+            //console.log("event.target.search-box.value: ",e.target.userInput.value)
+            let annoString = "" + `${e.target.userInput.value}`;
+            this.props.dispatch(setAnnotationString(annoString));
+            this.props.dispatch(annotate(annoString));
+        }
+        else {
+            e.preventDefault()
+            this.props.dispatch(clearAnnotation());
+        }
     }
 
     scrollToTop() {
@@ -57,8 +49,9 @@ export class NavBar extends React.Component {
     handleLogoutClick () {
         this.props.dispatch(logout());
     }
+
     handleGoToOtherPage() {
-        if(window.location.hash !== "#/dashboard" ){
+        if(!this.props.location.includes("dashboard")){
             window.location.hash="#/dashboard";
         }
         else {
@@ -71,66 +64,82 @@ export class NavBar extends React.Component {
         const props = this.props;
         let session = "";
 
-        console.log("window.location.hash", window.location.hash)
-
         if (this.props.isLoginForm && !this.props.userAuth)
             session = "Sign in"
         else if (!this.props.isLoginForm && !this.props.userAuth) {
             session = "Sign up"
         } 
         else {
-            session = `Logged in as ${this.props.user.username}`
+            session = `Logged in`
         }
 
         let location = "";
-        if (window.location.hash === "#/dashboard"){
+        if (this.props.location === "#/dashboard"){
             location = "Main"
         }
         else {
             location = "Dashboard"
         }
 
+                                    // <button type="button" onClick={this.handleHamburgerClick} className="button hamburger-link m-2">
+                                    //     <i className="fa fa-bars white-text" aria-hidden="true"></i>
+                                    // </button>
+                                    //<ClickOutHandler onClickOut={this.onClickOut}><span></span></ClickOutHandler>
+
         return (
                  ()=> {
-                    if (this.props.isTop===true && this.props.isCollapsed===true) {
+                    if (this.props.isTop===true && !this.props.location.includes("dashboard")) {
                         return (
                             <nav className="transparent">
                                 <div className="brand margin-top-14">
                                     <a onClick={this.scrollToTop} className="m-2 white-text navbar-brand link"><strong>CONTEXT</strong></a>
                                 </div>
                                     <ul className="white-text links">
-                                        <li><Link className="thistle-text-color link m-2" to={this.props.link1} spy={true} smooth={true} duration={750}>About</Link></li>
-                                        <li><Link className="thistle-text-color link m-2" to={this.props.link2} spy={true} smooth={true} duration={750}>Features</Link></li>
-                                        <li><Link className="thistle-text-color link m-2" to={this.props.link3} spy={true} smooth={true} duration={750}>{session}</Link></li>
+                                        <li><Link hidden={this.props.userAuth} className="thistle-text-color link m-2" to={this.props.link1} spy={true} smooth={true} duration={750}>About</Link></li>
+                                        <li><Link hidden={this.props.userAuth} className="thistle-text-color link m-2" to={this.props.link2} spy={true} smooth={true} duration={750}>Features</Link></li>
+                                        <li><Link hidden={this.props.userAuth} className="thistle-text-color link m-2" to={this.props.link3} spy={true} smooth={true} duration={750}>{session}</Link></li>
                                         <li className="thistle-text-color link m-2" hidden={!this.props.userAuth} onClick={this.handleGoToOtherPage}>{location}</li>
+                                        <button type="button" hidden={!this.props.userAuth} className="link button-link m-2 white-text float-flex-item-right" onClick={this.handleLogoutClick}>Logout ({this.props.user.username})</button>
                                     </ul>
-                                    <button type="button" hidden={this.props.userAuth} className="link button button-primary m-2">Sign In/Demo Account</button>
-                                    <button type="button" hidden={!this.props.userAuth} className="link button button-primary m-2" onClick={this.handleLogoutClick}>Logout</button>
-                                    <button type="button" className="button hamburger-link m-2">
-                                        <i className="fa fa-bars white-text" aria-hidden="true"></i>
-                                    </button>
+
                             </nav>
                         )
                     }
 
-                    else {
+                    else if (this.props.isTop===false && !this.props.location.includes("dashboard")) {
                         return (
                             <nav className="thistle-background-color">
                                 <div className="brand margin-top-14">
                                     <a onClick={this.scrollToTop} className="m-2 gray-text-color navbar-brand link"><strong>CONTEXT</strong></a>
                                 </div>
                                 <ul className="links dark-slate-gray-text-color">
-                                    <li><Link className="dark-slate-gray-text-color link m-2" to={this.props.link1} spy={true} smooth={true} duration={750}>About</Link></li>
-                                    <li><Link className="dark-slate-gray-text-color link m-2" to={this.props.link2} spy={true} smooth={true} duration={750}>Features</Link></li>
-                                    <li><Link className="dark-slate-gray-text-color link m-2" to={this.props.link3} spy={true} smooth={true} duration={750}>{session}</Link></li>
+                                    <li><Link hidden={this.props.userAuth} className="dark-slate-gray-text-color link m-2" to={this.props.link1} spy={true} smooth={true} duration={750}>About</Link></li>
+                                    <li><Link hidden={this.props.userAuth} className="dark-slate-gray-text-color link m-2" to={this.props.link2} spy={true} smooth={true} duration={750}>Features</Link></li>
+                                    <li><Link hidden={this.props.userAuth} className="dark-slate-gray-text-color link m-2" to={this.props.link3} spy={true} smooth={true} duration={750}>{session}</Link></li>
                                     <li className="dark-slate-gray-text-color link m-2" hidden={!this.props.userAuth} onClick={this.handleGoToOtherPage}>{location}</li>
+                                    <button type="button" onClick={this.handleLogoutClick} hidden={!this.props.userAuth} className="link button-link gray-text-color m-2 float-flex-item-right">Logout ({this.props.user.username})</button>  
                                 </ul>
-                                
-                                <button type="button"  hidden={this.props.userAuth} className="link button thistle-text-color light-sea-green-background-color m-2">Sign In/Demo Account</button>
-                                <button type="button" onClick={this.handleLogoutClick} hidden={!this.props.userAuth} className="link button thistle-text-color light-sea-green-background-color m-2">Logout</button>
                             </nav>
                         )
                     }
+                    else if (this.props.location.includes("dashboard")) {
+                        return (
+                            <nav className="thistle-background-color">
+                                <div className="brand margin-top-14">
+                                    <a onClick={this.scrollToTop} className="m-2 gray-text-color navbar-brand link"><strong>CONTEXT</strong></a>
+                                </div>
+                                <ul className="links dark-slate-gray-text-color">
+                                    <li className="dark-slate-gray-text-color link m-2" hidden={!this.props.userAuth} onClick={this.handleGoToOtherPage}>{location}</li>
+                                        <button type="button" onClick={this.handleLogoutClick} hidden={!this.props.userAuth} className="link button-link gray-text-color m-2">Logout ({this.props.user.username})</button>  
+                                    <form className="float-flex-item-right" id={this.props.formId} onSubmit={(e) => this.handleSubmit(e,this.props.annotation)}>
+                                        <button type="submit" className="button button-primary" name="search-button">{this.props.annotation===null ? "Annotate" : "Clear"}</button>
+                                    </form>
+                                </ul>
+
+                            </nav>
+                        )
+                    }
+                    
                 }
             )()
         }
@@ -140,11 +149,11 @@ const mapStateToProps = (state) => {
     
     return ({
          isTop: state.mainUi.isTop,
-         isCollapsed: state.mainUi.isCollapsed,
          isLoginForm: state.mainUi.isLoginForm,
          user: state.user.user,
          userAuth: state.user.userAuth,
-         location: state.user.location
+         location: state.user.location,
+         annotation:state.annotations.annotation
     })
 };
 
